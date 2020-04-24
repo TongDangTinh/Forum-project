ff0003528ca5ae4b35508f6f33acb2f337f3fa46
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-04-24 16:44:41 +0700

    Post discussion, display discustion on index

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,9 +1,26 @@
 class DiscussionsController < ApplicationController
-  
+
+  def index
+    @discussions = Discussion.all.order("created_at desc")
+  end
+
   def new
     @discussion = Discussion.new
   end
 
   def create
+    @discussion = Discussion.new discussion_params
+
+    if @discussion.save 
+      flash[:success] = "Discussion created!"
+      redirect_to root_path
+    else
+      render :new 
+    end
   end
+
+  private 
+    def discussion_params
+      params.require(:discussion).permit :title, :content
+    end
 end

dd91378ecfe8445f960a636a661a25abb34429c9
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-04-24 17:01:44 +0700

    Add user_id to discussions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_085155) do
+ActiveRecord::Schema.define(version: 2020_04_24_100037) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string "data_file_name", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_04_24_085155) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|

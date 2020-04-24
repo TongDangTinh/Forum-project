5de1d679842d3ef5ac80f8b667cd226b87d364af
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-04-24 16:58:52 +0700

    Discussion controller

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,7 +1,11 @@
 class DiscussionsController < ApplicationController
+  before_action :find_discussion, only: [:show, :edit, :update, :destroy]
 
   def index
     @discussions = Discussion.all.order("created_at desc")
+  end
+
+  def show
   end
 
   def new
@@ -18,9 +22,31 @@ class DiscussionsController < ApplicationController
       render :new 
     end
   end
+  
+  def edit
+  end
+
+  def update 
+
+    if @discussion.update_attribute discussion_params
+      flash[:success] = "Discussion updated!"
+      redirect_to discustion_path
+    else
+      render :edit
+    end
+  end
+
+  def distroy
+    @discussion.destroy
+  end
+
 
   private 
     def discussion_params
       params.require(:discussion).permit :title, :content
+    end
+
+    def find_discussion
+      @discussion = Discussion.find params[:id]
     end
 end

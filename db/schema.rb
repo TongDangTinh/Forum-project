67e393c24dee9f1a0d15ed260f39f22e83ffd38e
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-04-24 21:15:46 +0700

    Add user_id to relies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_140606) do
+ActiveRecord::Schema.define(version: 2020_04_24_141510) do
 
   create_table "channels", force: :cascade do |t|
     t.string "channel"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2020_04_24_140606) do
     t.text "reply"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "discussion_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
